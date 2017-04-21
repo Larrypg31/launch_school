50ab6a42ef811ff1aa0e899600bf98762e0dfc71
Larry Gladnick <larry.gladnick@gmail.com>
2017-04-21 17:08:49 -0400

    update

--- lesson_2/pseudo_code_test.rb
+++ lesson_2/pseudo_code_test.rb
@@ -1,28 +1,20 @@
 def conc_arr(arr)
-  string=""
-  arr.each_with_index do |s,i|
-    string << s
-    if i!=arr.length
-      string << " "
-    end
+  string = ''
+  arr.each_with_index do |word, indx|
+    string << word
+    string << ' ' if indx != arr.length
   end
   string
 end
 
-
-puts conc_arr(['my','dumb','string','dumb'])
-
+puts conc_arr(%w(my great wonderful array string'))
 
 def every_other(array)
-  new_array=[]
-  array.each_with_index do |v,i|
-    if (i%2==0)
-      new_array << v
-    end
+  new_array = []
+  array.each_with_index do |value, indx|
+    new_array << value if indx.even?
   end
   new_array
 end
 
-
-
-puts every_other([0,1,2,3,4,5,6,7,8])
+puts every_other([0, 1, 2, 3, 4, 5, 6, 7, 8])
